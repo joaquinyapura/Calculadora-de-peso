1dc830c3c88a4b605d0998b9640b1f23805e8dcc
Joaquin <joaquinyapura@gmail.com>
2025-02-22 21:13:18 -0300

    actualizacion de card para mostrar resultados

--- src/Components/Calculadora.jsx
+++ src/Components/Calculadora.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+import rect from "../assets/react.svg";
 
 export const Calculadora = () => {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+
+  const handleImput = (e) => {
+    setPeso(e.target.value);
+  };
+
+  const handleImputAltura = (e) => {
+    setAltura(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className=" ">
+    <div>
       <div className="w-[350px] mx-auto py-10 px-4 bg-white shadow-xl flex flex-col items-center">
         <h1 className="text-lg font-bold">Calculadora de peso corporal</h1>
-        <form action="" className="mt-5 flex flex-col gap-3">
+        <form
+          action=""
+          className="mt-5 flex flex-col gap-3"
+          onSubmit={handleSubmit}
+        >
           <div>
             <p className="font-semibold">peso</p>
             <input
               className="p-2 rounded-md border-gray-600 border-2 "
               type="text"
               placeholder="peso"
+              onChange={handleImput}
+              value={peso}
             />
           </div>
           <div>
@@ -20,13 +43,24 @@ export const Calculadora = () => {
               className="p-2 rounded-md border-gray-600 border-2"
               type="text"
               placeholder="altura"
+              onChange={handleImputAltura}
+              value={altura}
             />
           </div>
-          <button className="bg-slate-700 rounded-md  text-white p-2">
+          <button
+            type="submit"
+            className="bg-slate-700 rounded-md  text-white p-2"
+          >
             Sumbit
           </button>
           <button className="bg-slate-200 rounded-md  p-2">reload</button>
         </form>
+        <div className="flex flex-col items-center mt-4">
+          <h2>Su peso actual es</h2>
+          <p className="font-bold text-4xl">200</p>
+          <img src={rect} alt="" srcset="" className="w-20" />
+          <p>podrias </p>
+        </div>
       </div>
     </div>
   );
